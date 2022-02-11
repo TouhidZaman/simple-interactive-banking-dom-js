357a49157b4b596c12cab668cb78f1d05c741f7f
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-02-12 05:22:20 +0600

    deposit and withdraw handler code duplications removed

--- js/banking.js
+++ js/banking.js
@@ -1,45 +1,45 @@
+//for getting values
+const getValue = (inputId, isInput) => {
+    const inputField = document.getElementById(inputId);
+    if(isInput == true) {
+        let value = parseFloat(inputField.value);
+        inputField.value = '';
+        return value;
+    }
+    else return parseFloat(inputField.innerText);
+}
+
+const update = (fieldId, value) => document.getElementById(fieldId).innerText = value;
+
+//for updating Field value
+const updateFieldValue = (inputAmount, isWithdraw) => {
+    const balance = getValue('balance-field', false);
+    if(isWithdraw == true){
+        const prevWdroAmount = getValue('withdraw-field', false);
+        if(inputAmount > 0 && inputAmount < balance ) {
+            update('balance-field', balance - inputAmount);
+            update('withdraw-field', prevWdroAmount + inputAmount);
+        }
+    } 
+    else {
+        const prevDepoAmount = getValue('deposit-field', false);
+        if(inputAmount > 0) {
+            update('balance-field', balance + inputAmount);
+            update('deposit-field', prevDepoAmount + inputAmount);
+        }
+    }
+}
+
 //Deposit Handler
 const depositButton = document.getElementById('deposit-btn');
-
-depositButton.onclick = function () {
-    //Getting Deposit amount
-    const depositInput = document.getElementById('deposit-input');
-    const depositAmount = parseFloat(depositInput.value);
-
-    //Getting previous Deposit amount
-    const depositDisplayField = document.getElementById('deposit-field');
-    const prevDepoAmount = parseFloat(depositDisplayField.innerText);
-    
-    //Getting previous Total Balence
-    const balanceDisplayField = document.getElementById('balance-field');
-    const prevBalance = parseFloat(balanceDisplayField.innerText);
-
-    //Updating Total Deposit and Total Balence
-    if(depositAmount > 0) {
-        depositDisplayField.innerText = prevDepoAmount + depositAmount;
-        balanceDisplayField.innerText = prevBalance + depositAmount;
-    }
+depositButton.onclick = () => {
+    const depositAmount = getValue('deposit-input', true);
+    updateFieldValue(depositAmount, false);
 }
 
 //Withdraw Handler
 const withdrawButton = document.getElementById('withdraw-btn');
-
-withdrawButton.onclick = function () {
-    //Getting Withdraw amount
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmount = parseFloat(withdrawInput.value);
-
-    //Getting previous Withdraw amount
-    const withdrawDisplayField = document.getElementById('withdraw-field');
-    const prevWdroAmount = parseFloat(withdrawDisplayField.innerText);
-    
-    //Getting previous Total Balence
-    const balanceDisplayField = document.getElementById('balance-field');
-    const prevBalance = parseFloat(balanceDisplayField.innerText);
-
-    //Updating Total Withdraw and Total Balence
-    if(withdrawAmount > 0 && withdrawAmount < prevBalance) {
-        withdrawDisplayField.innerText = prevWdroAmount + withdrawAmount;
-        balanceDisplayField.innerText = prevBalance - withdrawAmount;
-    }
+withdrawButton.onclick = () => {
+    const withdrawAmount = getValue('withdraw-input', true);
+    updateFieldValue(withdrawAmount, true);
 }
